7b4b737f28f4daf22c3d05a86b7a4d3e133f31f4
bst-code <bsparksoftwaretacademy@gmail.com>
2025-10-26 16:11:01 +0530

    List and dropdown options

---
+++ Playwright_Concepts/tests/getAlloptionsFromDropdown.spec.ts
@@ -0,0 +1,21 @@
+import {test,expect} from "@playwright/test";
+
+
+test("Get All Option from Dropdown",async ({page})=>{
+
+await page.goto("https://playground.bsparksoftwaretechnologies.com/dropdown")
+
+const ele = page.locator("#bst_select1")
+await ele.selectOption({index: 4})
+
+
+//const optionElements = page.locator("//select[@id ='bst_select1']/option")
+const optionElements = await page.$$("//select[@id ='bst_select1']/option")
+
+for(let option of optionElements)
+{
+    console.log(await option.textContent());
+}
+
+await page.waitForTimeout(3000)
+})
---
+++ day5/ForEachConcept.js
@@ -0,0 +1,36 @@
+let student = "Bala"
+//console.log(student);
+
+// let Students = ["Bala", "John", "Vel","Vel"]
+// console.log(Students);
+// console.log(Students[1]);
+
+// Students.push("Shivya")
+
+// console.log(Students);
+
+// Students.pop()
+// console.log(Students);
+// Students.shift()
+// console.log(Students);
+
+// console.log(Students.length);
+
+let Students = ["Bala", "John", "Vel","Vel"]
+// for(let i = 0; i<Students.length; i++)
+// {
+//     console.log("Studnet name from list is :: ",Students[i]);
+    
+// }
+
+for (let std of Students)
+{
+    console.log(std); 
+}
+
+
+
+
+
+
+
