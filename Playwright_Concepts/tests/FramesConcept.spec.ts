fcf5da0ac13fe228a2d41ac5f8f09764de15fb65
bst-code <bsparksoftwaretacademy@gmail.com>
2025-11-01 17:41:59 +0530

    Multiple frames concept

--- Playwright_Concepts/tests/FramesConcept.spec.ts
+++ Playwright_Concepts/tests/FramesConcept.spec.ts
@@ -19,3 +19,21 @@ console.log(title);
 expect(title).toBe("Bala")
 
 })
+
+test.only("Frame Concept test 1", async({page}) =>{
+
+await page.goto("https://playground.bsparksoftwaretechnologies.com/frames")
+await page.frameLocator("//iframe[@id='bst_frame2']")
+          .frameLocator("//iframe[@id='bst_frame3']")
+          .locator('#nameInput')
+          .fill("Balamurugan")
+await page.waitForTimeout(3000)
+
+//multiple frame handled via page.frames() - it wont be used much in real scenarios
+const allFrames = page.frames()
+for(const fr of allFrames)
+{
+    fr.locator("#id").fill("Bala")
+}
+
+})
